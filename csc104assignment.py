8f1cad58627963a0435eed2d89644c389a236b77
Meeksani <81770891+Meeksani@users.noreply.github.com>
2021-04-04 09:45:11 +0100

    Add files via upload

---
+++ csc104assignment.py
@@ -0,0 +1,34 @@
+name = "SANI ISAH"
+department = "COMPUTER SCINCE"
+print(name)
+print (department)
+
+scare = "BOO"
+print(scare)
+
+v1 = "love"
+v2 = "coding"
+v3 = "v1 +v2"
+print(v1)
+print(v2)
+print(v3)
+
+
+
+division = 12 / .5
+print(division)
+
+
+num1 = 10
+num2 = 15
+print(num1+num2)
+
+
+x = 8
+y = 3
+print(8 % 3)
+
+
+x = 2 * 4
+y= 4 + 2
+print(x*y) 
